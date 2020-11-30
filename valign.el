06d9e1a287712e59a69ffcad5058a84431251a36
Yuan Fu <casouri@gmail.com>
2020-11-29 23:11:11 -0500

    Prepare for ELPA
    * .elpaignore: New file.
    * valign.el: Update file header.

--- valign.el
+++ valign.el
@@ -1,12 +1,29 @@
 ;;; valign.el --- Visually align tables      -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2019-2020 Free Software
+;; Foundation, Inc.
+
 ;; Author: Yuan Fu <casouri@gmail.com>
+;; Maintainer: Yuan Fu <casouri@gmail.com>
 ;; URL: https://github.com/casouri/valign
 ;; Version: 3.0.0
 ;; Keywords: convenience, text, table
 ;; Package-Requires: ((emacs "26.0"))
 
-;;; This file is NOT part of GNU Emacs
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
